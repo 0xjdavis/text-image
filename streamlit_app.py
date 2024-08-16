dbdc3a8c207e2e108ac838ec51d2b7fcbad2b9c3
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-15 21:20:19 -0700

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,29 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import requests
+
 openai_api_key = st.secrets["openai_key"]
 client=OpenAI(api_key=openai_api_key)
+
+st.sidebar.header("About App")
+
+# Calendly
+st.sidebar.markdown("""
+    <hr />
+    <center>
+    <div style="border-radius:8px;padding:8px;background:#fff";width:100%;">
+    <img src="https://avatars.githubusercontent.com/u/98430977" alt="Oxjdavis" height="100" width="100" border="0" style="border-radius:50%"/>
+    <br />
+    <span style="height:12px;width:12px;background-color:#77e0b5;border-radius:50%;display:inline-block;"></span> <b>I'm available for new projects!</b><br />
+    <a href="https://calendly.com/0xjavis" target="_blank"><button style="background:#126ff3;color:#fff;border: 1px #126ff3 solid;border-radius:8px;padding:8px 16px;margin:10px 0">Schedule a call</button></a><br />
+    </div>
+    </center>
+    <br />
+""", unsafe_allow_html=True)
+
+# Copyright
+st.sidebar.caption("©️ Copyright 2024 J. Davis")
+    
 
 st.title("OpenAI DALL-E Text-to-Image Generation") 
 st.caption("Prompted artwork powered by OpenAI DALL-E Model")
